e746298278768dac4f736cb2096b8205f0112fd3
moniquemtucker <moniquetucker@gmail.com>
2014-10-10 14:55:35 -0700

    added fees page, skeleton of blog, and js file

---
+++ coaching/coaching.js
@@ -0,0 +1,11 @@
+/**
+ * Created by monique tucker on 10/9/14.
+ */
+$(document).ready(function(){
+
+//fee structure is highlighted as cursor moves over it
+  $(".fees-plans").hover(function(){
+    $(this).toggleClass("active-fees-plans-head");
+  });
+
+});
