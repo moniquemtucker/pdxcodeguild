f0e4e6ef9bee9e605d1c706077003a3d02d90ad6
moniquemtucker <moniquetucker@gmail.com>
2014-10-12 15:59:16 -0700

    changing about me page layout

--- coaching/coaching.js
+++ coaching/coaching.js
@@ -4,8 +4,8 @@
 $(document).ready(function(){
 
 //fee structure is highlighted as cursor moves over it
-  $(".fees-plans").hover(function(){
-    $(this).toggleClass("active-fees-plans-head");
-  });
+    $(".fees-plans").hover(function(){
+        $(this).toggleClass("active-fees-plans-head");
+    });
 
 });
