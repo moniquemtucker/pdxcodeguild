d8557a03ca051beb2f63547b2a1f955368bcae5b
moniquemtucker <moniquetucker@gmail.com>
2014-10-14 21:25:48 -0700

    coaching site. general changes.

--- coaching/coaching.js
+++ coaching/coaching.js
@@ -3,6 +3,23 @@
  */
 $(document).ready(function(){
 
+//how it works process steps font increases as cursor moves over it
+//    $(".home-process-step").hover(function(){
+//        $(this).toggleClass("active-home-process-step");
+//    });
+
+    function scrollToCarousel(id){
+        id = id.replace("link", "");
+        $("html, body").animate({
+            scrollTop: $("#" + id).offset().top
+        }, 'slow')
+    }
+
+    $("#chevron").click(function(e) {
+        e.preventDefault();
+        scrollToCarousel("pp-carousel")
+    });
+
 //fee structure is highlighted as cursor moves over it
     $(".fees-plans").hover(function(){
         $(this).toggleClass("active-fees-plans-head");
